9084f0281e52d2ecc8887d424a7c897f4e9ecc7a
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 16:44:00 +0100

    changed code? 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 function App() {
 
   const bg = document.querySelector('.background-image');
-  const windowWidth = window.innerWidth / 5;
-  const windowHeight = window.innerHeight / 5 ;
+  // const windowWidth = window.innerWidth / 5;
+  // const windowHeight = window.innerHeight / 5 ;
 
   bg.addEventListener('mousemove', (e) => {
     e.preventDefault();
-    
+
     // // const mouseX = e.clientX / windowWidth;
     // const mouseY = e.clientY / windowHeight;
   
