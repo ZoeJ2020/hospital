8b8e0257de8c35b5fef84afdf94301d86d006790
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 15:24:40 +0100

    added app.js code for testing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 
 function App() {
 
-  // const bg = document.querySelector('.background-image');
-  // const windowWidth = window.innerWidth / 5;
-  // const windowHeight = window.innerHeight / 5 ;
+  const bg = document.querySelector('.background-image');
+  const windowWidth = window.innerWidth / 5;
+  const windowHeight = window.innerHeight / 5 ;
 
-  // bg.addEventListener('mousemove', (e) => {
-  //   const mouseX = e.clientX / windowWidth;
-  //   const mouseY = e.clientY / windowHeight;
+  bg.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX / windowWidth;
+    const mouseY = e.clientY / windowHeight;
   
-  //   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-  // });
+    bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+  });
 
   return (
     <div className="App">
