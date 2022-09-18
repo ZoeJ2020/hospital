279276ba3cc5e6d5a0ac9532ccf0166345255fda
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 16:40:21 +0100

    changed code?

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ function App() {
   const windowWidth = window.innerWidth / 5;
   const windowHeight = window.innerHeight / 5 ;
 
-  bg.addEventListener('mousemove', () => {
-    // const mouseX = clientX / windowWidth;
-    // const mouseY = clientY / windowHeight;
+  bg.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+    
+    // // const mouseX = e.clientX / windowWidth;
+    // const mouseY = e.clientY / windowHeight;
   
     // bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+
+    console.log('bg eventlistener is working');
   });
 
   return (
