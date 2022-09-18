dc911bb670bf1aee9798f190f34d6d40e5a43215
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 19:40:12 +0100

    Remove console log checks

--- src/App.js
+++ src/App.js
@@ -16,37 +16,11 @@ function App() {
       const mouseY = e.clientY / windowHeight;
     
       bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-
-      console.log('bg eventlistener is working');
     });
   })
-  console.log('JS has loaded');
-
-  const logLoad = (e) => {
-    console.log('body has loaded');
-
-  //   console.log(e);
-
-    //   const bg = document.querySelector('background-image');
-    //   const windowWidth = window.innerWidth / 5;
-    //   const windowHeight = window.innerHeight / 5 ;
-
-    // console.log(`${bg}, ${windowWidth}, ${windowHeight}`)
-
-      // bg.addEventListener('mousemove', (e) => {
-      //   e.preventDefault();
-
-      //   const mouseX = e.clientX / windowWidth;
-      //   const mouseY = e.clientY / windowHeight;
-      
-      //   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-
-      //   console.log('bg eventlistener is working');
-      // });
-  }
 
   return (
-    <div className="App" onLoad={logLoad()}>
+    <div className="App">
       <header className="App-header">
         <h1>Hover me</h1>
         <div className="background-image"></div>
