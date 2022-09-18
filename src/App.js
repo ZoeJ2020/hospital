1c7053f273c7c689f8b5fcba0dda5a37dd95b114
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 15:29:28 +0100

    test

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   const windowWidth = window.innerWidth / 5;
   const windowHeight = window.innerHeight / 5 ;
 
-  bg.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX / windowWidth;
-    const mouseY = e.clientY / windowHeight;
+  bg.addEventListener('mousemove', () => {
+    const mouseX = clientX / windowWidth;
+    const mouseY = clientY / windowHeight;
   
     bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
   });
