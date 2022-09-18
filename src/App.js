d74e31fc8f9a2881b994bebc1e37de9c6b3d7b2c
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 19:35:52 +0100

    ^7

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
+import React, { useRef, useEffect } from 'react'; //required for query selector work
+
 import './App.css';
 
 function App() {
 
+  useEffect(() => {
+    const bg = document.querySelector('.background-image');
+    const windowWidth = window.innerWidth / 5;
+    const windowHeight = window.innerHeight / 5 ;
+
+    bg.addEventListener('mousemove', (e) => {
+      e.preventDefault();
+
+      const mouseX = e.clientX / windowWidth;
+      const mouseY = e.clientY / windowHeight;
+    
+      bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+
+      console.log('bg eventlistener is working');
+    });
+  })
   console.log('JS has loaded');
 
-  // const bg = document.querySelector('.background-image');
-  // const windowWidth = window.innerWidth / 5;
-  // const windowHeight = window.innerHeight / 5 ;
+  // const logLoad = (e) => {
+  //   console.log('body has loaded');
 
-  // bg.addEventListener('mousemove', (e) => {
-  //   e.preventDefault();
+  //   console.log(e);
 
-    // const mouseX = e.clientX / windowWidth;
-    // const mouseY = e.clientY / windowHeight;
-  
-    // bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+    //   const bg = document.querySelector('background-image');
+    //   const windowWidth = window.innerWidth / 5;
+    //   const windowHeight = window.innerHeight / 5 ;
 
-  //   console.log('bg eventlistener is working');
-  // });
-
-  const logLoad = (e) => {
-    console.log('body has loaded');
-
-    console.log(e);
-
-      const bg = document.querySelector('background-image');
-      const windowWidth = window.innerWidth / 5;
-      const windowHeight = window.innerHeight / 5 ;
-
-    console.log(`${bg}, ${windowWidth}, ${windowHeight}`)
+    // console.log(`${bg}, ${windowWidth}, ${windowHeight}`)
 
       // bg.addEventListener('mousemove', (e) => {
       //   e.preventDefault();
@@ -40,7 +43,7 @@ function App() {
 
       //   console.log('bg eventlistener is working');
       // });
-  }
+  // }
 
   return (
     <div className="App" onLoad={logLoad()}>
