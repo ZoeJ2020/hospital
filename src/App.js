3cb4bb73796e8c89a75059fe8f248c390c03725e
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-09-18 18:21:07 +0100

    load tests

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import './App.css';
 
 function App() {
 
-  const bg = document.querySelector('.background-image');
+  console.log('JS has loaded');
+
+  // const bg = document.querySelector('.background-image');
   // const windowWidth = window.innerWidth / 5;
   // const windowHeight = window.innerHeight / 5 ;
 
-  bg.addEventListener('mousemove', (e) => {
-    e.preventDefault();
+  // bg.addEventListener('mousemove', (e) => {
+  //   e.preventDefault();
 
     // // const mouseX = e.clientX / windowWidth;
     // const mouseY = e.clientY / windowHeight;
   
     // bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
 
-    console.log('bg eventlistener is working');
-  });
+  //   console.log('bg eventlistener is working');
+  // });
+
+  const logLoad = () => {
+    console.log('body has loaded');
+  }
 
   return (
-    <div className="App">
+    <div className="App" onLoad={logLoad}>
       <header className="App-header">
         <h1>Hover me</h1>
         <div className="background-image"></div>
